1ad4e544227016beff9c2c915833d517f9bbce03
Xenock <miguelmoracho@gmail.com>
2017-04-17 17:40:43 +0200

    function canMove to ask if unit can move

--- spec/UnitSpec.js
+++ spec/UnitSpec.js
@@ -1,12 +1,13 @@
 describe('Unit', function(){
   var unit
-  var health, power, defense, position
+  var health, power, defense, position, movement
 
   beforeEach(function(){
     health = Math.floor((Math.random()*10 )+1)
     power = Math.floor((Math.random()*10 )+1)
     defense = Math.floor((Math.random()*10 )+1)
     position = {x:10, y:10}
+    movement = Math.floor((Math.random()*2 )+1)
     unit = new Unit(health, power, defense, position)
   })
 
@@ -17,6 +18,9 @@ describe('Unit', function(){
     it('should have a receive damage function', function(){
       expect(typeof(unit.receiveDamage)).toBe('function')
     })
+    it('should have a movement function', function(){
+      expect(typeof(unit.canMove)).toBe('function')
+    })
   })
 
   describe('Functions behaviour', function(){
@@ -24,9 +28,21 @@ describe('Unit', function(){
       it('should return power attribute', function(){
         expect(unit.attack()).toEqual(power)
       })
+    })
+    describe('receiveDamage', function(){
       it('should return actual health', function(){
         var actualHealth = health-(power-defense)
         expect(unit.receiveDamage(power)).toEqual(actualHealth)
+      })
+    })
+    describe('canMove', function(){
+      it('should permit to move less or equal than its movement', function(){
+        movement = 2
+        expect(unit.canMove(position.x+1, position.y+1) <= movement).toBe(true)
+      })
+      it('should not permit to move more than its movement', function(){
+        movement = 2
+        expect(unit.canMove(position.x+2, position.y+2) <= movement).toBe(false)
       })
     })
   })
--- src/Unit.js
+++ src/Unit.js
@@ -1,8 +1,9 @@
-function Unit(health, power, defense, position){
+function Unit(health, power, defense, position, movement){
   this.health = health
   this.power = power
   this.defense = defense
   this.position = position
+  this.movement = movement
 }
 
 Unit.prototype.attack = function(){
@@ -11,4 +12,8 @@ Unit.prototype.attack = function(){
 
 Unit.prototype.receiveDamage = function(damage){
   return this.health -= damage - this.defense
+}
+
+Unit.prototype.canMove = function(x, y){
+  return Math.abs(x - this.position.x) + Math.abs(y - this.position.y)
 }
