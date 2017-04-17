31df22eb87662b3086bccf6905815e1e65581bfb
Xenock <miguelmoracho@gmail.com>
2017-04-17 12:50:36 +0200

    jasmine test suite installed and initialized

---
+++ spec/UnitSpec.js
@@ -0,0 +1,12 @@
+describe('Unit', function(){
+  var unit
+
+  beforeEach(function(){
+    unit = new Unit()
+  })
+
+  describe('Attributes', function(){})
+  describe('Functions', function(){})
+  describe('Default attributes values'. function(){})
+  describe('Functions behaviour'. function(){})
+})
