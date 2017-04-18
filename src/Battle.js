5caaec84ae9f7fee7359bbc46367bdd997d1022a
Xenock <miguelmoracho@gmail.com>
2017-04-18 15:31:12 +0200

    initial board showed

--- src/Battle.js
+++ src/Battle.js
@@ -1,5 +1,21 @@
 var board
 
+function initialBoard(width, height){
+  return Array.apply(null, Array(width)).map(e => Array(height))
+}
+
 $(document).ready(function(){
-  
+  var body = $('body')
+  board = initialBoard(10, 20)
+
+  board.forEach(function(rowArray){
+    var row = $('<div>').addClass('row')
+
+    for(var i=0; i<rowArray.length; i++){
+      var column = $('<div>').addClass('column')
+      row.append(column)
+    }
+    body.append(row)
+  })
+
 })
