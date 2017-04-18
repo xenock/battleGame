b932eda1f0b3a9802c713841b53dcf175a55454b
Xenock <miguelmoracho@gmail.com>
2017-04-18 18:02:38 +0200

    painted one soldier

--- src/Battle.js
+++ src/Battle.js
@@ -1,15 +1,16 @@
 var board
+var unit = new Unit(10, 10, 5, {x: 5, y: 5}, 2)
 
 function initialBoard(width, height){
   return Array.apply(null, Array(width)).map(e => Array(height))
 }
 
 function paintBoard(board, body){
-  board.forEach(function(rowArray){
+  board.forEach(function(rowArray, xIndex){
     var row = $('<div>').addClass('row')
 
     for(var i=0; i<rowArray.length; i++){
-      var column = $('<div>').addClass('column')
+      var column = $('<div>').addClass('cell r'+xIndex+'c'+i)
       row.append(column)
     }
     body.append(row)
@@ -20,5 +21,6 @@ $(document).ready(function(){
   var body = $('body')
   board = initialBoard(10, 20)
   paintBoard(board, body)
-  
+  $('.r'+unit.position.x+'c'+unit.position.y).css('background', 'red')
+
 })
