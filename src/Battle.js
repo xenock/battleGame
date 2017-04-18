718a716fb8c83661152f3fc0042d3fd68eecbcc4
Xenock <miguelmoracho@gmail.com>
2017-04-18 20:34:19 +0200

    refactoring

--- src/Battle.js
+++ src/Battle.js
@@ -4,23 +4,27 @@ var unit = new Unit(10, 10, 5, {x: 5, y: 5}, 2, 'soldier')
 function initialBoard(width, height){
   return Array.apply(null, Array(width)).map(e => Array(height))
 }
-
 function paintBoard(board, body){
   board.forEach(function(rowArray, x){
     var row = $('<div>').addClass('row')
-
     for(var y=0; y<rowArray.length; y++){
-      var column = $('<div>').addClass('cell x'+x+'y'+y)
+      var column = $('<div>').addClass('cell x'+x+'y'+y).data({x:x, y:y})
       row.append(column)
     }
     body.append(row)
   })
+}
+function putUnitInMap(unit){
+  $('.x'+unit.position.x+'y'+unit.position.y)
+    .css('background', 'red')
+    .data('type', unit.type)
+    .on('click', function(){ console.log(unit.type)})
 }
 
 $(document).ready(function(){
   var body = $('body')
   board = initialBoard(10, 20)
   paintBoard(board, body)
-  $('.x'+unit.position.x+'y'+unit.position.y).css('background', 'red').data('type', unit.type).on('click', function(){ console.log(unit.type)})
+  putUnitInMap(unit)
 
 })
